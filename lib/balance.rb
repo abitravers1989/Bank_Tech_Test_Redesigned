4977139f981d54444b7636f9f1034dee88d5e328
Abi Travers <atravers4007@gmail.com>
2017-10-05 13:08:30 +0100

    Initalizing balance class with current balance which begins at 0

---
+++ lib/balance.rb
@@ -0,0 +1,10 @@
+#This class is responsible for storing a current balance, and for calculating the
+# balance when passed a credit or debit amount
+class Balance
+ attr_accessor :current_balance
+
+  def initialize
+    @current_balance = 0
+  end
+
+end
--- spec/balance_spec.rb
+++ spec/balance_spec.rb
@@ -1,11 +1,20 @@
-# require 'balance'
-#
-# describe Balance do
-#   let(:balance) { Balance.new }
-#
-#   context '#balance_calculation' do
-#     it 'Calculated the balance when passed the amount withdrawn or deposited' do
-#
-#     end
-#   end
-# end
+require 'balance'
+
+describe Balance do
+  let(:balance) { Balance.new }
+  let (:credit) { double Credit.new(10) }
+
+  context '#initialize' do
+    it 'the current balance starts at zero' do
+      expect(balance.current_balance).to eq 0
+    end
+  end
+
+  # context '#balance_calculation' do
+  #   it 'Calculated the balance when passed the amount withdrawn or deposited' do
+  #      balance takes credit.credit_amount and deducts this amount from the balance
+  #      balance.balance_calculation(credit.credit_amount)
+  #      and returns balance.balance_amount curent balance - credit.credit_amount
+  #   end
+  # end
+end
