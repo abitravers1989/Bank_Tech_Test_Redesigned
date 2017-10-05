e564e3a489e27602e93bc9fbaca742eb5b356150
Abi Travers <atravers4007@gmail.com>
2017-10-05 11:26:09 +0100

    Testing that an instance of the DateNow class has a method called createdate and this method runs

--- spec/dateNow_spec.rb
+++ spec/dateNow_spec.rb
@@ -8,11 +8,8 @@ describe DateNow do
 
   context '#createdate' do
     it 'testing that the class has the method createdate which runs' do
-      expect(date).to receive(:today).and_return("04/04/2017")
-      p date
-      p subject.createdate
-      # here it seems i am just testing the actual method. not the method with the double inside it.
-      expect(subject.createdate).to eq "04/04/2017"
+      allow(date).to receive(:today).and_return("04/04/2017")
+      subject.createdate
     end
   end
 end
