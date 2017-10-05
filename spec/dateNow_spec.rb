3888618ce8016e509fb67b9ec46ba27fdbcf7f10
Abi Travers <atravers4007@gmail.com>
2017-10-05 11:14:59 +0100

    Issues around date double not actually being put into method when it is ran. Creating a record of my thought process in working this out

--- spec/dateNow_spec.rb
+++ spec/dateNow_spec.rb
@@ -1,17 +1,20 @@
 require 'dateNow.rb'
 require 'spec_helper'
+require 'Date'
 
 describe DateNow do
-  let (:date) { double :date }
-  subject {DateNow.new(date)}
+  subject { DateNow.new }
+  let (:date) { double Date }
 
   context '#createdate' do
-    it 'creates a date for today' do
-      expect(date).to recieve{:today}
-      date.createdate
+    it 'testing that the class has the method createdate which runs' do
+      # it is not using the double
+      allow(date).to receive(:today).and_return("04/04/2017")
+      p date
+      p.date.today
+      p subject.createdate
+      # doesn't seem to use the double of date but instead it creates a new instance of date
+      expect(subject.createdate).to eq "04/04/2017"
     end
   end
 end
-
-
-# .and_return("04/10/2017")
