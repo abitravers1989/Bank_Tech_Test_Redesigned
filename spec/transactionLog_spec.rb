8c76a8c0020a13fc9b457b03141107a4a6de9cdd
Abi Travers <atravers4007@gmail.com>
2017-10-06 14:12:27 +0100

    Transaction Log class tested and complete

--- spec/transactionLog_spec.rb
+++ spec/transactionLog_spec.rb
@@ -1,19 +1,20 @@
-# require 'transactionLog.rb'
-#
-# describe TransactionLog do
-#   let(:transaction_log) { TransactionLog.new }
-#
-#   context '#initialize' do
-#     it 'Creates an account array which will hold all the transactions' do
-#       expect(transaction_log.account).to be_instance_of Array
-#       expect(transaction_log.account).to be_empty
-#     end
-#   end
-#
-#   context '#transaction_string' do
-#     it 'Creates a string which reprsents a transaction' do
-#        expect(transaction_log.transaction_string).to be_instance_of String
-#     end
-#   end
-#
-# end
+require 'transactionLog.rb'
+
+describe TransactionLog do
+  let(:transaction_log) { TransactionLog.new }
+
+  context '#initialize' do
+    it 'Creates an account array which will hold all the transactions' do
+      expect(transaction_log.account_transactions).to be_instance_of Array
+      expect(transaction_log.account_transactions).to be_empty
+    end
+  end
+
+  context '#createing_transaction_array' do
+    it 'Creates a string which reprsents a transaction' do
+       transaction_log.createing_transaction_array("05/10/2017", 10, 0, 10)
+       expect(transaction_log.account_transactions).to_not be_empty
+    end
+  end
+
+end
